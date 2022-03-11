4de752e5bad7e4a6f963a4b26062f4afc233fc4f
alexruizlul <alex007ruiz@gmail.com>
2022-03-11 16:35:15 -0600

    [ADD] upstash redis

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           <a href="https://nextjs.org">Next.js</a> Shopify App Boilerplate
